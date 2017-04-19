c1635d9a4131d972d47c5e037cf3edb6f0c15cfb
suibian <suibian>
2017-04-19 10:52:24 +0800

    学校找大招会！！

--- src/main/java/com/ezd/Controller/app/BigretController.java
+++ src/main/java/com/ezd/Controller/app/BigretController.java
@@ -39,5 +39,17 @@ public class BigretController {
         return bigretList;
     }
 
+    /**
+     * 点击某个学校
+     * 然后就进入到该学校的大招会列表
+     * @param id
+     * @return
+     */
+    @RequestMapping("/schoolebigret")
+    @ResponseBody
+    public List<EzdBigret> getschoolBigret(int id){
+        List<EzdBigret> bigretList = ezdBigretService.addressGet(id);
+        return bigretList;
+    }
 }
 
